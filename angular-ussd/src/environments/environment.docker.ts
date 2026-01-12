b5bd9b0eb9c54065e70d406bb825341f8bebf6dd
bsadjetey <bsadjetey@gmail.com>
2026-01-12 14:58:22 +0100

    update gateway endpoint

--- angular-ussd/src/environments/environment.docker.ts
+++ angular-ussd/src/environments/environment.docker.ts
@@ -3,6 +3,6 @@ import { baseEnvironment } from './environment.base';
 export const environment = {
   production: true,
   // apiBaseUrl: 'http://api.ussd365.com/api/v1/', // Use Docker service name
-  apiBaseUrl: 'http://gateway.ussd365.com/api/v1/', // Use Docker service name
+  apiBaseUrl: 'https://gateway.ussd365.com/api/v1/', // Use Docker service name
   ...baseEnvironment
 };
