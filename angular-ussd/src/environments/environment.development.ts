49a1d7bb83d085b581807355d9acdc4368e3eebb
bsadjetey <bsadjetey@gmail.com>
2026-01-12 08:02:58 +0100

    add deploy workflow

--- angular-ussd/src/environments/environment.development.ts
+++ angular-ussd/src/environments/environment.development.ts
@@ -2,6 +2,7 @@ import { baseEnvironment } from "./environment.base";
 
 export const environment = {
     production: false,
+    // apiBaseUrl: 'http://api.ussd.localhost/api/v1/', // Use Docker service name
     apiBaseUrl: 'http://localhost:8000/api/v1/',
     ...baseEnvironment,
 };
